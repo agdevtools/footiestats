a4f345ad838f211f719f54bc1cfca11a6a4f2f37
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-06-12 14:58:37 +0100

    delete application yaml and add application properties local and prod

--- src/main/kotlin/com/footiestats/FootiestatsApplication.kt
+++ src/main/kotlin/com/footiestats/FootiestatsApplication.kt
@@ -7,6 +7,22 @@ import org.springframework.boot.runApplication
 class FootiestatsApplication
 
 fun main(args: Array<String>) {
+	setOperatingSystem()
 	runApplication<FootiestatsApplication>(*args)
 	print("Running Footie Stats...")
+}
+
+
+fun getOperatingSystem(): String {
+	val os = System.getProperty("os.name")
+	println("Using System Property: $os")
+	return os
+}
+
+fun setOperatingSystem() {
+	if (getOperatingSystem().toLowerCase().contains("mac")) {
+		System.setProperty("spring.profiles.active", "local")
+	} else {
+		System.setProperty("spring.profiles.active", "prod")
+	}
 }
