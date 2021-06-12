288965661efb538c5b97be7e8168fbd7b9f70ba5
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-06-12 15:39:44 +0100

    add jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,48 @@
+node {
+    def gitRepository = 'https://github.com/agdevtools/footiestats.git/'
+    def gitBranch = '*/master'
+    def githubcreds = [
+            $class      : 'UsernamePasswordMultiBinding',
+            credentialsId : 'githubcreds',
+            usernameVariable : 'GIT_USER',
+            passwordVariable : 'GIT_PASS'
+    ]
+
+    stage('Clean Workspace') {
+        cleanWs()
+    }
+
+    stage('Git Checkout') {
+        withCredentials([githubcreds]){
+            checkout([
+                    $class      : 'GitSCM',
+                    branches    : [[name:"${gitBranch}"]],
+                    doGenerateSubModuleConfigurations : false,
+                    extensions: [],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId  : 'githubcreds',
+                                         url            :"${gitRepository}"]]
+
+            ])
+
+        }
+    }
+
+    stage('Build') {
+
+        sh './gradlew clean build -x test'
+
+    }
+
+    stage('Test') {
+
+        sh './gradlew clean test --info'
+
+    }
+
+    stage('Deploy to Heroku') {
+
+        sh './gradlew build deployHeroku'
+
+    }
+}
