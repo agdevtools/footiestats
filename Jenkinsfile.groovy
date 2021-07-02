c4baf7aefb7d1f71d7bf66d79fe5a267e46433f2
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-07-03 00:16:21 +0100

    add pager duty integration

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -33,7 +33,7 @@ node {
         sh './gradlew clean build -x test'
         sh 'curl -H "Content-Type: application/json" --request POST \\\n' +
                 '  --url https://events.pagerduty.com/v2/change/enqueue \\\n' +
-                '  --data \'{"payload":{"summary":"From Jenkins","timestamp":"2021-07-02T11:15:22Z","source":"string","custom_details":{}},"routing_key":"e429be3c8ea94c06c0429c80719dd399","links":[{"href":"string","text":"string"}]}\''
+                '  --data \'{"payload":{"summary":"From Jenkins","source":"string","custom_details":{}},"routing_key":"e429be3c8ea94c06c0429c80719dd399","links":[{"href":"string","text":"string"}]}\''
 
     }
 
