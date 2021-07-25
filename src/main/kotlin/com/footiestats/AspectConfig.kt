fe2153b13bb686d87f895b05be545bb174df117a
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-07-25 22:13:46 +0100

    add RabbitMQ functionality

--- src/main/kotlin/com/footiestats/AspectConfig.kt
+++ src/main/kotlin/com/footiestats/AspectConfig.kt
@@ -3,6 +3,7 @@ package com.footiestats
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
@@ -19,9 +20,13 @@ class AspectConfig {
     class ControllerLoggingAllAdvice {
         val logger = Logger.getLogger(AspectConfig::class.java.name)
 
+        @Autowired
+        val rabbitMqProducer = RabbitMqProducer();
+
         @Before("execution(public * com.footiestats.controller.FootieController.*(..))")
         fun logController(joinPoint: JoinPoint) {
             logger.info("Logging controller  $joinPoint  with no arg ")
+            rabbitMqProducer.sendMessage("Received request for $joinPoint")
         }
     }
 
--- src/main/kotlin/com/footiestats/RabbitMqProducer.kt
+++ src/main/kotlin/com/footiestats/RabbitMqProducer.kt
@@ -4,22 +4,17 @@ import com.rabbitmq.client.ConnectionFactory
 import org.springframework.stereotype.Service
 import java.nio.charset.StandardCharsets
 
+
 @Service
 class RabbitMqProducer {
 
-    fun main() {
+    fun sendMessage(message : String) {
         val factory = ConnectionFactory()
-        factory.newConnection("amqps://qlmyewbx:OBj45osHeb2TD5PEVOewGBoTnml_C1wb@squid.rmq.cloudamqp.com/qlmyewbx").use { connection ->
+        factory.newConnection("amqps://cvofxaso:bh2MdB29UtcNXf7b2rwEB4gXGbmho9fV@squid.rmq.cloudamqp.com/cvofxaso").use { connection ->
             connection.createChannel().use { channel ->
                 channel.queueDeclare("footie-stats-test-queue", false, false, false, null)
-                val message = "Hello World!"
-                channel.basicPublish(
-                    "",
-                    "footie-stats-test-queue",
-                    null,
-                    message.toByteArray(StandardCharsets.UTF_8)
-                )
-                println(" [x] Sent '$message'")
+                channel.basicPublish("", "footie-stats-test-queue", null, message.toByteArray(StandardCharsets.UTF_8))
+               // println(" [x] Sent '$message'")
             }
         }
     }
